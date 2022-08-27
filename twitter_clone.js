ac2533a591083eab0077663f3f960521ded16364
Sauravsharma2025 <sauravsharma2025@gmail.com>
2022-08-27 15:42:16 +0530

    Single Tweet Deletion Implemented

--- twitter_clone.js
+++ twitter_clone.js
@@ -1,19 +1,38 @@
  //var tweet_img=document.getElementById('file_input').value
    // console.log(tweet_img) 
-    //p.innerHTML=tweet
+    //p.innerHTML=tweet 
   var ele = document.querySelector('#twtbtn')  
   var Element = document.querySelector('#file_input');
+  
   var img = document.querySelector('#imgtwt');
   ele.addEventListener("click",abc)
   function abc(){
+   if(Element.files.length!==0){
+   console.log(Element.files[0])
+   
     var url = URL.createObjectURL(Element.files[0]);
+
+    console.log("abc")
+    console.log(url)
     img.src = url;
+   
+
+   }
+
+  
+      
+   
+   
+    
     var tweet= document.getElementById('tweet_data').value
     let p = document.createElement('div')
-
+   
     //document.getElementById('tweeting').innerHTML=tweet
     document.getElementById('boiler_div').appendChild(p)
-    var done =`
+    document.getElementById('tweet_data').value='' 
+
+    var done1 =`
+    <div id='twth'>
     <div  id="twt_div"class="post" style="display:flex ;">
     <div class="post_avatar">
         <img src="/images/man.png" alt="avatar">
@@ -25,14 +44,27 @@
                 <span class="post_headerSpecial">
                 <span class="material-icons post__badge">verified</span>@Saurav_Sharma
                 </span>
+                <div class="remove_twt">
+                <button id='dd'><span id='rmvtwt' class="material-icons">
+close
+</span></button></div>
                 </h3>
             </div>
             <div class="post_headerDescription">
                 <p id="tweeting">${tweet}</p>
             </div>
-        </div>
-        <img src="${url}" alt=""/>
-        <div class="post_footer">
+        </div>`
+        if(url!=null){
+
+       var done3= `<img src="${url}" alt=""/>`
+       url=''
+        }
+        else{
+            done3=''
+          
+        }
+        
+       var done2=` <div class="post_footer">
             <span class="material-icons">reply</span>
             <span class="material-icons">publish</span>
             <span class="material-icons">favorite border</span>
@@ -40,11 +72,20 @@
 
         </div>
     </div>
-</div> `
+</div></div>`
+
+var done=done1+done3+done2
 p.innerHTML=done
-document.getElementById('tweet_data').value=''    
+var reomvetwt=document.getElementById('dd');
+reomvetwt.addEventListener("click",function(event){
+    var removed=event.target
+    removed.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove()
+})
+
 }
- 
+
+
+
 
    
    
