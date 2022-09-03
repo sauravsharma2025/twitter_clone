fd8f4ba8c4b0a4958da57562a4781f0551fceba9
Sauravsharma2025 <sauravsharma2025@gmail.com>
2022-09-04 03:08:46 +0530

    Tweet stroing  implemented and redirect createpage to home page once done note:deleting tweet still not fixed

--- twitter_clone.js
+++ twitter_clone.js
@@ -1,10 +1,6 @@
- //var tweet_img=document.getElementById('file_input').value
-   // console.log(tweet_img) 
-    //p.innerHTML=tweet 
-  var ele = document.querySelector('#twtbtn')
-   
+ 
+  var ele = document.querySelector('#twtbtn') 
   var Element = document.querySelector('#file_input');
-  
   var img = document.querySelector('#imgtwt');
   ele.addEventListener("click",abc)
   function abc(){
@@ -19,10 +15,23 @@
    
     //document.getElementById('tweeting').innerHTML=tweet
     document.getElementById('boiler_div').appendChild(p)
-    document.getElementById('tweet_data').value='' 
+    document.getElementById('tweet_data').value='' //reseting box
+
    let current_user=localStorage.getItem(document.getElementById('user_id').value)
    let current_user_obj=JSON.parse(current_user);
-   console.log(current_user_obj.name);
+   let count=current_user_obj.count; //this counter help to track tweets of user
+   
+   count++;
+   
+    //Storing User Tweeets
+
+    current_user_obj[count] = tweet;//adding tweet into the user storage
+    current_user_obj["count"]=count;//how many number of tweets in tht session
+    //logic for adding image
+    if(url!=''){
+    console.log(url);}
+    localStorage.setItem(document.getElementById('user_id').value,JSON.stringify(current_user_obj))
+
     var done1 =`
     <div id='twth'>
     <div  id="twt_div"class="post" style="display:flex ;">
@@ -66,25 +75,83 @@ close
 
 var done=done1+done3+done2
 p.innerHTML=done
-
-
 }
 
-document.body.addEventListener('click',function(event){
-    if(event.target.id=='rmvtwt'){
-        var removed=event.target
-        removed.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove()
-        
-    }
-})
   function trigger(){
   let user_id = document.querySelector('#user_id').value;
   let pwd=document.querySelector('#pwd').value;
  // localStorage.setItem(user_id,user_id)  
  // localStorage.setItem(pwd,pwd)
- profile=localStorage.getItem(user_id);
- let profile_obj=JSON.parse(profile);
+  let profile=localStorage.getItem(user_id);//according to user object aaa jayega
+ let profile_obj=JSON.parse(profile);//creating object from localstorage string
  
+ 
+ //Older Tweeet Restore
+ //
+//console.log(profile_obj.count)
+for( var i=1;i<=profile_obj.count;i++){
+var tweet=profile_obj[i];
+
+
+ if(Element.files.length!==0){
+   
+    var url = URL.createObjectURL(Element.files[0]);//only accepting var??
+  // URL.revokeObjectURL(Element.files[0]);
+    img.src = url;
+    
+   }
+ 
+ var part1 =`
+ <div id='twth'>
+ <div  id="twt_div"class="post" style="display:flex ;">
+ <div class="post_avatar">
+     <img src="images/man.png" alt="avatar">
+ </div>
+ <div class="post_body">
+     <div class="post_header">
+         <div class="post_headerText">
+        
+             <h3><span id="tweet_user">${profile_obj.name+" "+profile_obj.last}</span>
+             <span class="post_headerSpecial">
+             <span class="material-icons post__badge">verified</span>@${profile_obj.name+profile_obj.last}
+             </span>
+             <div class="remove_twt">
+             <button id='dd'><span id='rmvtwt' class="material-icons">
+close
+</span> <input type='hidden' value='${i}' class='twtnb'><!--this is used for getting tweet id--></button></div>
+             </h3>
+         </div>
+         <div class="post_headerDescription">
+             <p id="tweeting">${tweet}</p>
+            
+         </div>
+     </div>`
+     
+     if(url!=null){
+
+    var part3= `<img src="${url}" alt=""/>`
+    
+     }
+     else{
+         part3=''
+       
+     }
+    var part2=` <div class="post_footer">
+         <span class="material-icons">reply</span>
+         <span class="material-icons">publish</span>
+         <span class="material-icons">favorite border</span>
+     </div>
+ </div>
+</div></div>`
+
+let part=part1+part3+part2;
+
+let v = document.createElement('div')
+document.getElementById('boiler_div').appendChild(v)
+v.innerHTML=part; }
+
+
+
 if(user_id==profile_obj.name && pwd==profile_obj.password){
     modal.style.display="none";
 }
@@ -94,10 +161,18 @@ var btn = document.getElementById('trigger');
 var span= document.getElementsByClassName('close')[0];
  modal.style.display="block";
 
-
- //making username dyanmic
+//Logic for removing tweet;
+document.body.addEventListener('click',function(event){
+    if(event.target.id=='rmvtwt'){
+        var removed=event.target
+        removed.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove()
+        
+        let remove_str=localStorage.getItem(document.getElementById('user_id').value);
+        let remove_obj=JSON.parse(remove_str);
+       
+            
+        
+        
+    }
+})
  
-
-
-   
-   
