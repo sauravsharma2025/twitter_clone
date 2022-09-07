7fcbaf01ee733674799051ee1f766dbd5479b1f8
Sauravsharma2025 <sauravsharma2025@gmail.com>
2022-09-07 18:52:49 +0530

    Login validation added

--- images/twitter_clone.js
+++ images/twitter_clone.js
@@ -88,41 +88,13 @@ var notification = new Notification(title, { body, icon });
 }
 
   function trigger(){
-   
   let user_id = document.querySelector('#user_id').value;
   let pwd=document.querySelector('#pwd').value;
-  let profile=localStorage.getItem(user_id);//according to user object aaa jayega
-  let profile_obj=JSON.parse(profile);//creating object from localstorage string
-  if(profile==undefined){
-    alert("Please register before login")
-  }
-  let regex = /^[A-Za-z0-9 ]+$/;
-  let isValid=regex.test(user_id);
-  if(user_id==''||pwd==''){
-    alert("Both field are required")
-  }
- 
-  else if(user_id.length>8){
-    alert("UserId should be less than 8 char" );
-    document.getElementById('user_id').style.border='1px solid red'
-  }
-  else if(pwd !=profile_obj.password){
-    alert("Password is incorrect!")
-    document.getElementById('pwd').value=''
-    document.getElementById('pwd').style.border='1px solid red'
-  }
- 
-
-  else if(!isValid){
-    alert("ohhh bhai")
-    document.getElementById('user_id').style.border='1px solid red'
-  }
-
-  else{
  // localStorage.setItem(user_id,user_id)  
  // localStorage.setItem(pwd,pwd)
+  let profile=localStorage.getItem(user_id);//according to user object aaa jayega
+ let profile_obj=JSON.parse(profile);//creating object from localstorage string
  
- console.log("hi",localStorage.getItem(user_id))
  
  //Older Tweeet Restore
 
@@ -193,7 +165,7 @@ if(user_id==profile_obj.name && pwd==profile_obj.password){
     modal.style.display="none";
     
 }
-  }}
+  }
 var modal=document.getElementById('Login');
 var btn = document.getElementById('trigger');
 var span= document.getElementsByClassName('close')[0];
@@ -216,28 +188,3 @@ document.body.addEventListener('click',function(event){
 Notification.requestPermission().then(function (permission) {
     console.log(permission);
 });
-
-//Enabling Login button
-/*
-$('#user_id').on('keyup',stateHandle)
-function stateHandle(e){
-    var txt=$('user_id').value;
-    var rex=txt.replace(/ /g,'')
-    if(reg.length==0){
-        $('#user_id').attr('disabled',true)
-    }
-    else{
-        $('#user_id').removeAttr('disabled');
-    }
-}
-*/
-function enabling(){
-    let xd=document.getElementById('user_id').value;
-    var rex=xd.replace(/ /g,'');
-    if(rex.length==0){
-       alert("Please Enter detail")
-    }
-    else{
-        document.getElementById('xyz').removeAttribute('disabled')
-    }
-}
